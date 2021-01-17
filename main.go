e2bef83a81b53a878b7cce7cf7112a32136683a3
Vitaly Kezlya <vitaly@kezlya.com>
2021-01-17 13:04:34 +0300

    simplification

--- main.go
+++ main.go
@@ -41,48 +41,53 @@ type Point struct {
 
 func main() {
 	// starting listen for http calls on port :7070
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// your bot respons should be json object
-		w.Header().Set("content-type", "application/json")
-
-		data, _ := ioutil.ReadAll(r.Body)
-		defer r.Body.Close()
-		var req Request
-		_ = json.Unmarshal(data, &req)
-
-		// available actions and directions
-		var actions = []string{"stay", "move", "eat", "take", "put"}
-		var directions = []string{"up", "down", "right", "left"}
-		response := Response{
-			Orders: make([]Order, 0),
-		}
-
-		// loop through ants and give orders
-		for _, ant := range req.Ants {
-			order := Order{
-				AntId:     ant.Id,
-				Action:    actions[rand.Intn(4)],    // pick random action from array on line 54
-				Direction: directions[rand.Intn(3)], // pick random direction from array on line 55
-			}
-
-			// add order to your response object from line 20
-			response.Orders = append(response.Orders, order)
-		}
-
-		bytes, _ := json.Marshal(response)
-		w.Write(bytes)
-
-		// fmt.Println(string(bytes))
-		// {"orders": [
-		//	 {"antId":1,"act":"move","dir":"down"},
-		//	 {"antId":17,"act":"load","dir":"up"}
-		//	]}
-	})
-
+	http.HandleFunc("/", handleAllRequests)
 	err := http.ListenAndServe(":7070", nil)
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
+}
+
+func handleAllRequests(w http.ResponseWriter, r *http.Request) {
+	// your bot response should be json object
+	w.Header().Set("content-type", "application/json")
+
+	// unmarshal request payload into objects
+	data, _ := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	var req Request
+	_ = json.Unmarshal(data, &req)
+
+	// available actions and directions
+	// var actions = []string{"stay", "move", "eat", "take", "put"}
+	var directions = []string{"up", "down", "right", "left"}
+
+	orders := []Order{}
+	// loop through ants and give random move order
+	for _, ant := range req.Ants {
+		order := Order{
+			AntId:  ant.Id,
+			Action: "move",
+			// pick random direction from slice on line 62
+			Direction: directions[rand.Intn(3)],
+		}
+		orders = append(orders, order)
+	}
+
+	// prepare response object
+	response := Response{Orders: orders}
+
+	bytes, _ := json.Marshal(response)
+	_, err := w.Write(bytes)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+
+	// fmt.Println(string(bytes)) // your json response should like this
+	// {"orders": [
+	//	 {"antId":1,"act":"move","dir":"down"},
+	//	 {"antId":17,"act":"load","dir":"up"}
+	//	]}
 }
 
 // this code available at https://github.com/anthive/go
